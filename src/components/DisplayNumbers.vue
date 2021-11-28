4947c5fdf9ad4f48dfbf3cd8ab74d6ebb0f70610
howard chen <jump7755331@gmail.com>
2021-11-28 15:12:18 +0800

    project init

---
+++ src/components/DisplayNumbers.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>{{ storeState }}</h2>
+    </div>
+</template>
+
+<script>
+import { store } from '../store.js'
+
+export default {
+    data(){
+        return {
+            storeState: store.state.numbers
+        }
+    }
+}
+
+</script>
